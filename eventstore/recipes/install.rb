806f34631e3461d8dbbad8bc8ef7a64844c4f84e
Ryan J. Melena <Ryan.Melena@noesislabs.com>
2015-08-14 15:43:03 -0500

    Fix typos in evenstore install.rb

--- eventstore/recipes/install.rb
+++ eventstore/recipes/install.rb
@@ -1,10 +1,10 @@
 powershell_script 'install eventstore' do
   code <<-EOH
   Add-Type -assembly "system.io.compression.filesystem"
-  wget "#{node[:evenstore][:url]}" -OutFile "%TEMP%\\eventstore.zip"
-  [io.compression.zipfile]::ExtractToDirectory("%TEMP%\\eventstore.zip", "#{node[:evenstore][:destination_path]}")
+  wget "#{node[:eventstore][:url]}" -OutFile "%TEMP%\\eventstore.zip"
+  [io.compression.zipfile]::ExtractToDirectory("%TEMP%\\eventstore.zip", "#{node[:eventstore][:destination_path]}")
   choco install nssm --acceptlicense --yes --force
-  nssm install EventStore "#{node[:evenstore][:destination_path]}\\EventStore.ClusterNode.exe" --db ./db --log ./logs
+  nssm install EventStore "#{node[:eventstore][:destination_path]}\\EventStore.ClusterNode.exe" --db ./db --log ./logs
   EOH
   action :run
 end
