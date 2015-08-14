1d6d8320909304fbe405602a49df09fa73d31c18
Ryan J. Melena <Ryan.Melena@noesislabs.com>
2015-08-14 15:56:24 -0500

    Fix powershell environment variable syntax

--- eventstore/recipes/install.rb
+++ eventstore/recipes/install.rb
@@ -1,8 +1,8 @@
 powershell_script 'install eventstore' do
   code <<-EOH
   Add-Type -assembly "system.io.compression.filesystem"
-  wget "#{node[:eventstore][:url]}" -OutFile "%TEMP%\\eventstore.zip"
-  [io.compression.zipfile]::ExtractToDirectory("%TEMP%\\eventstore.zip", "#{node[:eventstore][:destination_path]}")
+  wget "#{node[:eventstore][:url]}" -OutFile "$env:TEMP\\eventstore.zip"
+  [io.compression.zipfile]::ExtractToDirectory("$env:TEMP\\eventstore.zip", "#{node[:eventstore][:destination_path]}")
   choco install nssm --acceptlicense --yes --force
   nssm install EventStore "#{node[:eventstore][:destination_path]}\\EventStore.ClusterNode.exe" --db ./db --log ./logs
   EOH
