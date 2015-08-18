616817933ead401b260cd2874ccd038b661bb057
Ryan J. Melena <Ryan.Melena@noesislabs.com>
2015-08-18 13:26:46 -0500

    Fix variable name typo, Fix ServerInstance argument syntax

--- ssrs/recipes/configure.rb
+++ ssrs/recipes/configure.rb
@@ -1,10 +1,10 @@
-service 'ReportServer' do
+windows_service 'ReportServer' do
   action [:start, :enable]
 end
 
 powershell_script "Configure SSRS" do
   code <<-EOH
-  $rsConfig = Get-WmiObject -namespace "root\\Microsoft\\SqlServer\\ReportServer\\RS_MSSQLServer\\v12\\Admin" -class MSReportServer_ConfigurationSetting -ComputerName localhost -filter "InstanceName='#{node[:ssrsdbsetup][:instance_name]}'"
+  $rsConfig = Get-WmiObject -namespace "root\\Microsoft\\SqlServer\\ReportServer\\RS_MSSQLServer\\v12\\Admin" -class MSReportServer_ConfigurationSetting -ComputerName localhost -filter "InstanceName='#{node[:ssrsconfig][:instance_name]}'"
 
   $rsConfig.RemoveURL("ReportServerWebService", "#{node[:ssrsconfig][:base_url]}", 1033);
   $rsConfig.RemoveURL("ReportManager", "#{node[:ssrsconfig][:base_url]}", 1033);
@@ -16,7 +16,7 @@ powershell_script "Configure SSRS" do
   $rsConfig.ReserveURL("ReportManager", "#{node[:ssrsconfig][:base_url]}", 1033);
 
   $script = $rsConfig.GenerateDatabaseCreationScript("#{node[:ssrsconfig][:database_name]}", 1033, $FALSE)
-  Invoke-Sqlcmd -Query $script -ServerInstance "#{node[:ssrsconfig][:instance_name]}"
+  Invoke-Sqlcmd -Query $script -ServerInstance "localhost\\#{node[:ssrsconfig][:instance_name]}"
 
   EOH
   action :run
