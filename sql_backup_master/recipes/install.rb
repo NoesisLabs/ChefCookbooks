968abd5ad7092d83c9c65bd7d46cfc762a246928
Ryan J. Melena <Ryan.Melena@noesislabs.com>
2015-08-18 10:16:48 -0500

    Casing consistency

--- sql_backup_master/recipes/install.rb
+++ sql_backup_master/recipes/install.rb
@@ -2,7 +2,7 @@ require 'win32/service'
 
 powershell_script 'install sql backup master' do
   code <<-EOH
-  wget "#{node[:sqlBackupMaster][:url]}" -OutFile "$env:TEMP\\sqlbackupmaster.exe"
+  wget "#{node[:sqlbackupmaster][:url]}" -OutFile "$env:TEMP\\sqlbackupmaster.exe"
   & $env:TEMP\\sqlbackupmaster.exe /quiet
   EOH
   action :run
