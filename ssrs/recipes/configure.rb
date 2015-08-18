bb0182740c06052d2ee4354582ee26d737ea9b51
Ryan J. Melena <Ryan.Melena@noesislabs.com>
2015-08-18 13:39:15 -0500

    Remove semi-colons

--- ssrs/recipes/configure.rb
+++ ssrs/recipes/configure.rb
@@ -6,14 +6,14 @@ powershell_script "Configure SSRS" do
   code <<-EOH
   $rsConfig = Get-WmiObject -namespace "root\\Microsoft\\SqlServer\\ReportServer\\RS_MSSQLServer\\v12\\Admin" -class MSReportServer_ConfigurationSetting -ComputerName localhost -filter "InstanceName='#{node[:ssrsconfig][:instance_name]}'"
 
-  $rsConfig.RemoveURL("ReportServerWebService", "#{node[:ssrsconfig][:base_url]}", 1033);
-  $rsConfig.RemoveURL("ReportManager", "#{node[:ssrsconfig][:base_url]}", 1033);
+  $rsConfig.RemoveURL("ReportServerWebService", "#{node[:ssrsconfig][:base_url]}", 1033)
+  $rsConfig.RemoveURL("ReportManager", "#{node[:ssrsconfig][:base_url]}", 1033)
 
-  $rsConfig.SetVirtualDirectory("ReportServerWebService", "ReportServer", 1033);
-  $rsConfig.SetVirtualDirectory("ReportManager", "Reports", 1033);
+  $rsConfig.SetVirtualDirectory("ReportServerWebService", "ReportServer", 1033)
+  $rsConfig.SetVirtualDirectory("ReportManager", "Reports", 1033)
 
-  $rsConfig.ReserveURL("ReportServerWebService", "#{node[:ssrsconfig][:base_url]}", 1033);
-  $rsConfig.ReserveURL("ReportManager", "#{node[:ssrsconfig][:base_url]}", 1033);
+  $rsConfig.ReserveURL("ReportServerWebService", "#{node[:ssrsconfig][:base_url]}", 1033)
+  $rsConfig.ReserveURL("ReportManager", "#{node[:ssrsconfig][:base_url]}", 1033)
 
   $script = $rsConfig.GenerateDatabaseCreationScript("#{node[:ssrsconfig][:database_name]}", 1033, $FALSE)
   Invoke-Sqlcmd -Query $script -ServerInstance "localhost\\#{node[:ssrsconfig][:instance_name]}"
