ad905448b89593a3208a2f9f6b7fa38672f1da6d
Ryan J. Melena <Ryan.Melena@noesislabs.com>
2015-08-18 09:58:51 -0500

    Fix ssrs_db_setup delimiter escapes

--- ssrs_db_setup/recipes/install.rb
+++ ssrs_db_setup/recipes/install.rb
@@ -1,8 +1,12 @@
 require 'win32/service'
 
+service 'ReportServer' do
+  action [:start, :enable]
+end
+
 powershell_script "Create SSRS Database" do
   code <<-EOH
-  $rsConfig = Get-WmiObject -namespace "root\Microsoft\SqlServer\ReportServer\RS_MSSQLServer\v12\Admin" -class MSReportServer_ConfigurationSetting -ComputerName localhost -filter "InstanceName='#{node[:ssrsdbsetup][:instance_name]}'"
+  $rsConfig = Get-WmiObject -namespace "root\\Microsoft\\SqlServer\\ReportServer\\RS_MSSQLServer\\v12\\Admin" -class MSReportServer_ConfigurationSetting -ComputerName localhost -filter "InstanceName='#{node[:ssrsdbsetup][:instance_name]}'"
   $script = $rsConfig.GenerateDatabaseCreationScript("#{node[:ssrsdbsetup][:database_name]}", 1033, $FALSE)
   Invoke-Sqlcmd -Query $script -ServerInstance "#{node[:ssrsdbsetup][:instance_name]}"
   EOH
