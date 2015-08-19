89345d1ed7712922a53e63b53220db8524231bae
Ryan J. Melena <Ryan.Melena@noesislabs.com>
2015-08-19 16:07:09 -0500

    Fix chocolatey guard definition, Fix Octopus chocolatey path environment reference

--- chocolatey/recipes/install.rb
+++ chocolatey/recipes/install.rb
@@ -1,4 +1,6 @@
 powershell_script 'Install Chocolatey' do
   code "iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))"
-  not_if "choco -h"
+  convert_boolean_return true
+  guard_interpreter :powershell_script
+  not_if "Try{& $env:CHOCOLATEYINSTALL\\choco.exe -h; Return $TRUE;}Catch{Return $FALSE;}"
 end
