f4272165e111541d49381ed812a46eb2c6d9b05b
Shannon Kasper <shannon.kasper@noesislabs.com>
2015-08-26 10:49:29 -0500

    Change cert creation logic

--- ssl_cert/recipes/install.rb
+++ ssl_cert/recipes/install.rb
@@ -1,22 +1,20 @@
 powershell_script 'Install SSL Certificate' do
   code <<-EOH
-  $certBytes = [System.Convert]::FromBase64String("#{node[:ssl_cert][:base_64_cert]}")
-  $cert = New-Object System.Security.Cryptography.X509Certificates.X509Certificate2($certBytes, "#{node[:ssl_cert][:cert_password]}", "MachineKeySet,PersistKeySet")
-  $cert.FriendlyName = "#{node[:ssl_cert][:cert_friendly_name]}"
-  $store = New-Object System.Security.Cryptography.X509Certificates.X509Store("#{node[:ssl_cert][:store_name]}", "#{node[:ssl_cert][:store_location]}")
-  $store.Open("ReadWrite")
-  $store.Add($cert)
-  $store.Close()
+  $file = [System.IO.Path]::Combine([System.IO.Path]::GetTempPath(), "chef-" + [System.Guid]::NewGuid())
+  Try
+    {
+      [System.IO.File]::WriteAllBytes($file, [System.Convert]::FromBase64String("#{node[:ssl_cert][:base_64_cert]}"))
+      $cert = New-Object System.Security.Cryptography.X509Certificates.X509Certificate2($file, "#{node[:ssl_cert][:cert_password]}", "MachineKeySet,PersistKeySet")
+      $cert.FriendlyName = "#{node[:ssl_cert][:cert_friendly_name]}"
+      $store = New-Object System.Security.Cryptography.X509Certificates.X509Store("#{node[:ssl_cert][:store_name]}", "#{node[:ssl_cert][:store_location]}")
+      $store.Open("ReadWrite")
+      $store.Add($cert)
+      $store.Close()
+    }
+  Finally
+    {
+      [System.IO.File]::Delete($file)
+    }
   EOH
   action :run
-  convert_boolean_return true
-  guard_interpreter :powershell_script
-  not_if <<-EOH
-  $store = New-Object System.Security.Cryptography.X509Certificates.X509Store("#{node[:ssl_cert][:store_name]}", "#{node[:ssl_cert][:store_location]}")
-  $store.Open("ReadOnly")
-  $certNames = $store.Certificates | Select FriendlyName
-  $store.Close()
-  Return ($certNames -contains "#{node[:ssl_cert][:cert_friendly_name]}")
-  EOH
 end
-
