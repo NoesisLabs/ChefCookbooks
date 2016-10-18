9e702125d31775928c122637ef700b44c0b061cd
Ryan J. Melena <Ryan.Melena@noesislabs.com>
2016-10-18 10:05:34 -0500

    Eliminate nesting from Mirth Connect attributes

--- mirth_connect/recipes/install.rb
+++ mirth_connect/recipes/install.rb
@@ -53,10 +53,10 @@ end
 
 
 # Download and setup Mirth
-downloaded_archive = "#{node[:mirthconnect][:homedir]}/#{node[:mirthconnect][:download_file]}"
+downloaded_archive = "#{node[:mirthconnect][:homedir]}/mirthconnect-#{default[:mirthconnect][:version]}-unix.tar.gz"
 remote_file downloaded_archive do
   user node[:mirthconnect][:user]
-  source node[:mirthconnect][:download_url]
+  source "http://downloads.mirthcorp.com/connect/#{default[:mirthconnect][:version]}/mirthconnect-#{default[:mirthconnect][:version]}-unix.tar.gz"
   not_if { File.exists? downloaded_archive }
 end
 
